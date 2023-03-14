86a7364a52b0bf56380e97b025c4f856f839afbd
michelleh11 <107732329+michelleh11@users.noreply.github.com>
2023-03-14 16:52:36 -0700

    Delete js directory

--- js/javascript.js
+++
@@ -1,9 +0,0 @@
-function instructionClose () {
-
-    document.getElementById('instructionOverlay').classList.remove('show');
-    document.getElementById('instructionOverlay').classList.add('hidden');
-}
-
-$(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  })
